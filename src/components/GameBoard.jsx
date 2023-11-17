a919860f1d38dd22ceece0d316f3881c44825097
Frostyflames12 <rizqyriadi02@gmail.com>
2023-11-17 16:55:39 +0700

    Adding logs and updated GameBoard
    log added, and now button cannot be clicked if already clicked

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,3 @@
-
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -9,11 +7,9 @@ const initialGameBoard = [
 export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initialGameBoard;
 
-
   for (let turn of turns) {
-
-    const {square, player} = turn;
-    const {row, col} = square;
+    const { square, player } = turn;
+    const { row, col } = square;
 
     gameBoard[row][col] = player;
   }
@@ -39,7 +35,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
